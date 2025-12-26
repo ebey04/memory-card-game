d035e4838f6a296421ad919eb5d6acc8abadf575
Elizabeth Bey <ebey04@gmail.com>
2025-12-26 13:16:10 -0500

    feat(Header): create Header component with h1 & game instructions. add background-img to index.css

---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export default function Header () {
+    return (
+        <div className="background">
+            <h1>Remember That Tamale!</h1>
+            <p>Click on one tamale, but never the same one again.</p>
+            <p>See how high of a streak you can get.</p>
+        </div>
+    )
+}
