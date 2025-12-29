3450b9ede7c4de26c29b2b05d1e2f6a2ae76acda
Elizabeth Bey <ebey04@gmail.com>
2025-12-29 09:43:24 -0500

    feat(Message): add aria-live for screen reader announcement

--- src/Message/Message.jsx
+++ src/Message/Message.jsx
@@ -3,7 +3,7 @@ import './message.css'
 export default function Message({streak, showMessage}) {
 
     return (
-        <div className= {`message ${showMessage ? '' : 'hidden'}`}>
+        <div className= {`message ${showMessage ? '' : 'hidden'}`} aria-live= "assertive">
             Oh no! You clicked that tamale already. Highest streak is {streak} tamales.
         </div>
     )
